1878c0e37a3ceed709144d0a68e088de97d47f5e
郭明锋 <gmf520@yeah.net>
2026-05-11 00:22:28 +0800

    15

--- src/apps/com.gamestar.pianoperfect.ts
+++ src/apps/com.gamestar.pianoperfect.ts
@@ -6,12 +6,24 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '开屏广告',
+      name: '开屏广告1',
       enable: false,
       rules: [
         {
           matches: [
             '@TextView < FrameLayout <2 FrameLayout <2 FrameLayout <2 FrameLayout < FrameLayout < FrameLayout < [vid="splash_container"]',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '开屏广告2',
+      enable: false,
+      rules: [
+        {
+          matches: [
+            '@View <3 FrameLayout <2 FrameLayout < FrameLayout < [vid="splash_container"]',
           ],
         },
       ],
