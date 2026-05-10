344cb40a4c8b23426207d493c65a034188e33638
郭明锋 <gmf520@yeah.net>
2026-05-10 14:06:22 +0800

    更新多看规则

--- src/apps/com.duokan.reader.ts
+++ src/apps/com.duokan.reader.ts
@@ -8,10 +8,29 @@ export default defineGkdApp({
       key: 1,
       name: '正文底部广告',
       enable: false,
-      actionMaximum: 1,
       rules: [
         {
-          matches: ['[id="reading__bottom_ad_view__root"]'],
+          matches: ['[vid="reading__app_ad_view__close"]'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '正文全屏广告关闭',
+      enable: false,
+      rules: [
+        {
+          matches: ['[vid="reading__app_ad_view__close"]'],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '正文全屏广告反馈',
+      enable: false,
+      rules: [
+        {
+          matches: ['[vid="ad__feedback_close_dialog_button"]'],
         },
       ],
     },
---
+++ src/apps/com.miui.systemAdSolution.ts
@@ -0,0 +1,28 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.miui.systemAdSolution',
+  name: '智能服务广告',
+  groups: [
+    {
+      key: 1,
+      name: '为什么不希望看到此条内容',
+      enable: false,
+      rules: [
+        {
+          matches: ['[vid="no_interest"]'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '不感兴趣',
+      enable: false,
+      rules: [
+        {
+          matches: ['[vid="item_feedback"][text="广告密集"]'],
+        },
+      ],
+    },
+  ],
+});
