6ad8fef832db98b7efc62154fcb8ca4f16458aaf
郭明锋 <gmf520@yeah.net>
2026-05-10 23:26:41 +0800

    3

--- src/subscription.ts
+++ src/subscription.ts
@@ -5,11 +5,11 @@ import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
   id: 233,
-  name: 'Subscription',
+  name: 'gmf520-gkd',
   version: 0,
-  author: 'author',
+  author: '郭明锋',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/gmf520/gkd-rules',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
